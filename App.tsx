fc6ee681d078e9c5c628807fc32c675662b356cd
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2024-02-21 08:43:19 +0100

    feat: nesting BottomTabNavigator into StackScreenNavigator

--- App.tsx
+++ App.tsx
@@ -14,20 +14,48 @@ import Recherche from './screens/Recherche.tsx';
 import Equipe from './screens/Equipe.tsx';
 import Parametres from './screens/Parametres.tsx';
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-function App(): React.JSX.Element {
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Accueil" component={Home} />
+      <Tab.Screen name="Recherche" component={Recherche} />
+      <Tab.Screen name="Équipe" component={Equipe} />
+      <Tab.Screen name="Paramètres" component={Parametres} />
+    </Tab.Navigator>
+  );
+};
+
+const StackScreenNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="BottomTabs"
+        component={BottomTabNavigator}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Accueil"
+        component={Home}
+        options={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+        }}
+      />
+      {/* Ajoutez d'autres écrans empilables ici si nécessaire */}
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Accueil" component={Home} />
-        <Tab.Screen name="Recherche" component={Recherche} />
-        <Tab.Screen name="Équipe" component={Equipe} />
-        <Tab.Screen name="Paramètres" component={Parametres} />
-      </Tab.Navigator>
+      <StackScreenNavigator />
     </NavigationContainer>
   );
-}
+};
 
 export default App;
